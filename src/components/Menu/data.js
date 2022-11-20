71614cd0d81fe828d620f00b31ce8c6923d73933
datt7e4 <datt7e4@gmail.com>
2022-11-20 14:47:57 -0800

    fixed image bug2

--- src/components/Menu/data.js
+++ src/components/Menu/data.js
@@ -2,6 +2,6 @@ export const menu_data = [
   { title: "Bánh Lọc", item_price: "$60 / 100 cái", desc: "", img_src: "./images/banh-loc.jpg" },
   { title: "Bánh Lọc Chay", item_price: "$50/ 100 cái", desc: "", img_src: "./images/banh-loc-chay.jpg" },
   { title: "Bánh Nậm", item_price: "$80 / 100 cái", desc: "", img_src: "./images/banh-nam.jpg" },
-  { title: "Bánh Ít Mặn", item_price: "$70 / 100 cái", desc: "", img_src: "./images/banh-it.jpeg" },
+  { title: "Bánh Ít Mặn", item_price: "$70 / 100 cái", desc: "", img_src: "./images/banh-it.jpg" },
   { title: "Bánh Gói", item_price: "$50 / 100 cái", desc: "", img_src: "./images/banh-goi.jpg" },
 ]
