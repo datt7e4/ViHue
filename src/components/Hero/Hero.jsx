329418bd1a7915371b708db4100e81a2c2292d8c
datt7e4 <83128851+datt7e4@users.noreply.github.com>
2022-12-29 12:57:28 -0800

    fix bugs v2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import { Button, ThemeProvider, Typography } from "@mui/material";
+import { ThemeProvider, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import { useContext } from "react";
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import { LanguageContext } from "../../App";
-import { Container, Grid, ThemeProvider, Typography } from "@mui/material";
+import { Container, Grid, Typography, ThemeProvider } from "@mui/material";
 import { getMenuData, getMenuDescription } from "../constants/menu";
+import { theme } from "../Hero/style";
 import "./Menu.css";
-import { menuDescriptionConst } from "../constants/constant";
 
 function Menu() {
   const isEnglish = useContext(LanguageContext);
@@ -11,43 +11,45 @@ function Menu() {
   const menu_description = getMenuDescription(isEnglish);
 
   return (
-    <Container
-      id={isEnglish ? "menu" : "thuc-don"}
-      align="center"
-      maxWidth="xl"
-    >
-      <Typography variant="h3" sx={{ margin: "2rem" }}>
-        Menu
-      </Typography>
-      <Typography>{menu_description}</Typography>
-      <Grid container marginTop="3rem" marginBottom="3rem">
-        {menu_data.map((item) => {
-          return (
-            <Grid
-              key={item.title}
-              marginBottom="3rem"
-              item
-              xs={12}
-              // sm={6}
-              // md={4}
-              // lg={3}
-            >
-              <img
-                // style={{ borderRadius: "50%" }}
-                src={item.img_src}
-                alt="logo"
-                className="menu-img"
-                // width={'50%'}
-              />
-              <Typography variant="h6" fontWeight="bold">
-                {item.title}
-              </Typography>
-              <Typography variant="body2">{item.item_price}</Typography>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container
+        id={isEnglish ? "menu" : "thuc-don"}
+        align="center"
+        maxWidth="xl"
+      >
+        <Typography variant="h3" sx={{ margin: "2rem" }}>
+          {isEnglish ? "Menu" : "Thực đơn"}
+        </Typography>
+        <Typography variant="body1">{menu_description}</Typography>
+        <Grid container marginTop="3rem" marginBottom="3rem">
+          {menu_data.map((item) => {
+            return (
+              <Grid
+                key={item.title}
+                marginBottom="3rem"
+                item
+                xs={12}
+                // sm={6}
+                // md={4}
+                // lg={3}
+              >
+                <img
+                  // style={{ borderRadius: "50%" }}
+                  src={item.img_src}
+                  alt="logo"
+                  className="menu-img"
+                  // width={'50%'}
+                />
+                <Typography variant="h6" fontWeight="bold">
+                  {item.title}
+                </Typography>
+                <Typography variant="body2">{item.item_price}</Typography>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
